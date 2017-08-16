9a2d9cfbc8161bf7f7ca84768a86d8d3fda3cccd
Seong Ma <sjma1@uci.edu>
2017-08-16 16:40:08 -0700

    Add files via upload

--- functions.py
+++ functions.py
@@ -12,13 +12,32 @@ def get_directory():
 def get_encryption():
     return input("Enter the encryption key: ")
 
+
+def append_to_filename(filename):
+    return filename.split('.pdf')[0] + '_encrypted.pdf'
+
+def write_to_pdf(new_file, pdf_writer, pdf_reader):
+    for page_number in range(pdf_reader.numPages):
+        pdf_writer.addPage(pdf_reader.getPage(page_number))
+    pdf_writer.write(new_file)
+
+
 def create_encrypted_pdfs(encryption_key, directory):
     for dir_name, subdir_list, file_list in os.walk(directory):
         for filename in file_list:
             if filename.endswith('.pdf'):
-                
-                
-                
+                                
                 absolute_directory = os.path.abspath(dir_name)
                 absolute_filename  = os.path.join(absolute_directory, filename)
-                send2trash.send2trash(absolute_filename)
+                absolute_file      = open(absolute_filename, 'rb')
+                pdf_reader = PyPDF2.PdfFileReader(absolute_file)
+
+                if not pdf_reader.isEncrypted:
+                    pdf_writer = PyPDF2.PdfFileWriter()
+                    os.chdir(absolute_directory)
+                    new_file = open(append_to_filename(filename), 'wb')
+                    pdf_writer.encrypt(encryption_key)
+                    write_to_pdf(new_file, pdf_writer, pdf_reader)
+                    new_file.close()
+                    absolute_file.close()
+                    send2trash.send2trash(absolute_filename)
--- main.py
+++ main.py
@@ -9,10 +9,12 @@ and a password, and will then walk through the directory
 and its subdirectories and encrypt all found pdf files 
 with the password, encrypted files will contain the same
 filename but with _encrypted appended. The original files
-will be sent to the recycle bin.
+will be sent to the recycle bin. Does not work on files that
+are already encrypted
 '''
 
 if __name__ == '__main__':
     directory = functions.get_directory()
-    
-    
+    key       = functions.get_encryption()
+    functions.create_encrypted_pdfs(key, directory)
+    print("Completed")
